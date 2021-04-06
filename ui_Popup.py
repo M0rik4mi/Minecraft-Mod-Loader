31ef808f618741236db1dc373e077fc9df10eb40
M0rik4mi <nairama@web.de>
2021-04-06 22:36:50 +0200

    main

--- ui_Popup.py
+++ ui_Popup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'PopupulYyXa.ui'
+## Form generated from reading UI file 'PopupDnfSnl.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.0.2
 ##
